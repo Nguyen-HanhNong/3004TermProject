236d0bf34c68966fbbdee8cc71afb8b81a2119d0
AshokSivathayalan <120435769+AshokSivathayalan@users.noreply.github.com>
2023-04-04 21:31:05 -0400

    LED, Lightstrip, and Battery
    Header and implementation for Lightstrip, LED, and Battery UI elements

---
+++ Header Files/battery.h
@@ -0,0 +1,37 @@
+#ifndef BATTERY_H
+#define BATTERY_H
+
+#include <QObject>
+#include <QWidget>
+#include <QLabel>
+#include <QSlider>
+#include <QTimer>
+
+class Battery: public QWidget
+{
+    Q_OBJECT
+public:
+    Battery(QWidget* = nullptr);
+
+    int getBattery(){return percent;}
+
+
+signals:
+    //Used to indicate that the device has no charge left and should not function
+    void batteryDrained();
+    //Used to indicate that the device has been charged and can now function again
+    void batteryCharged();
+
+
+public slots:
+    void updateBattery();
+    void drainBattery();
+
+private:
+    QLabel* percentLabel; //UI element to show user how much battery is left
+    QSlider* bSlider; //UI element to let user recharge battery
+    QTimer timer; //Used to continuously drain battery
+    int percent; //Stores current charge level for simplicity
+};
+
+#endif // BATTERY_H
