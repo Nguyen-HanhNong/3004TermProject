d307f724db8bafbb4e287895b5abb32bc8ab91e7
Shreya Voore <87192140+shreyavooree@users.noreply.github.com>
2023-04-13 11:55:41 -0400

    mainwindow.h comments

--- 3004ProjTest/mainwindow.h
+++ 3004ProjTest/mainwindow.h
@@ -1,3 +1,8 @@
+/*
+    Authors: Shreya Voore, Nguyen-Hanh Nong, Ashok Sivathalayan, Awwab Mahdi
+    Purpose: The main window of the application, intializes and controls the application.
+*/
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -27,54 +32,58 @@ private:
     Ui::MainWindow *ui;
 
     //menu related
-    Menu* mainMenu;
-    Menu* parentMenu;
+    Menu* mainMenu; //the menu the user is currently on
+    Menu* parentMenu; //the initial/original menu node, for destructor
 
-    QListWidget *curQListWidget;
-    void updateMenu(const QString, QVector<QString>);
-    void initializeMainMenu(Menu*);
+    QListWidget *curQListWidget; //the list widget the menus are displayed on
+    void updateMenu(const QString, QVector<QString>); //updates the list widget with the selected menu items
+    void initializeMainMenu(Menu*); //initializes the main menu, sets up the menu hierarchy for the user
 
     //challenge level
-    void setChallengeLevel(int);
+    void setChallengeLevel(int); //sets the challenge level for the next sessions
 
     //reset device
     void resetDevice();
 
     //log/history
-    void viewLog(int);
+    void viewLog(int); //view all the information pertaining to a session
 
     //breath pacer
-    void setBreathPacer();
-    void moveBPSliderLeft();
-    void moveBPSliderRight();
-    void updateSliderText();
+    void setBreathPacer(); //displays the breath pacer slider widget when selected
+    void updateSliderText(); //changes the slider text depending on what percent the slider is set to
 
-    //session stuff
-    QVector<Session*>* sessions;
-    static int currentSession;
-    int nextChallengeLevel;
+    //session
+    QVector<Session*>* sessions; //vector containing all the sessions recorded
+    static int currentSession; //used for the id of a session
+    int nextChallengeLevel; //the challenge level of the next session
     QTimer *plottingTimer;
-    bool *updateSessionGraph;
-    bool activeSession;
-    bool on;
-    bool hasSignal;
+    bool *updateSessionGraph; //updates the session graph
+    bool activeSession; //signals whether a session is happening or not currently
+    bool on; //boolean that indicates whether or not the device is on
+    bool hasSignal; //indicates whether the hrv signal is on or not
 
-    void addNewSession();
-    void startSession();
-    void endSession();
-    void returnToMenu();
-    void deleteSession();
+    void addNewSession();//adds a new session to the session list
+    void startSession();//starts a session
+    void endSession();//ends a session
+    void returnToMenu();//once the session is over, upon clicking the return to menu button, displays main menu
+    void deleteSession();//deletes a session from the menu tree and the session vector
 
-    void turnOn();
-    void turnOff();
+    void turnOn();//turns device on
+    void turnOff();//turns device off
 
 private slots:
+    //Menu slots
     void upButton();
     void downButton();
     void select();
     void menu();
     void goBack();
 
+    //Breath Pacer slots
+    void moveBPSliderLeft();
+    void moveBPSliderRight();
+
+    //Changes signal
     void changeSignal();
 
     //Battery slots
