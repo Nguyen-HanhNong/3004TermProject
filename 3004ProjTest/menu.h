2bdc4586d10392aa3de7984a98c18c5274cf2e68
Shreya Voore <87192140+shreyavooree@users.noreply.github.com>
2023-04-13 11:53:48 -0400

    commented menu.h

--- 3004ProjTest/menu.h
+++ 3004ProjTest/menu.h
@@ -1,3 +1,10 @@
+/*
+    Contributor: Shreya Voore
+    File name: menu.cpp
+    Purpose: Tree object/data structure that is used to store menu information to be displayed and manipulated on the main window
+    Source: Adapted from the Denas Case Study Menu class, from Brightspace (COMP3004)
+*/
+
 #ifndef MENU_H
 #define MENU_H
 
@@ -11,26 +18,28 @@ class Menu
 public:
     explicit Menu(QString, QVector<QString> itemList, Menu*);
     ~Menu();
-    QString getName();
-    QVector<QString> getMenuItems();
-    Menu* getParentMenu();
-    int getPosition();
-    Menu* get(int);
-    void addChildMenu(Menu*);
-    int getLength();
 
-    QVector<Menu*> getChildMenus();
-    void addNewMenuItem(QString item);
+    //getters
+    QString getName(); //gets the name of the menu
+    QVector<QString> getMenuItems(); //returns the menuItems of a menu as QStrings
+    Menu* getParentMenu(); //gets the parent menu of the menu
+    int getPosition(); //gets the position of the menu in the menu list
+    Menu* get(int); //gets the menu at that index in the list
+    int getLength(); //gets the number of child menus in the menu
+    QVector<Menu*> getChildMenus(); //returns all the menu items as Menu* objects
 
-    void removeMenuItem(int removeIndex);
+    //add and removes
+    void addChildMenu(Menu*); //adds a child menu as a Menu*
+    void addNewMenuItem(QString item); //adds a new menu item from a QString
+    void removeMenuItem(int removeIndex); //removes a menu item at the given index
 
 private:
-    QString menuName;
-    QVector<QString> menuItems;
-    int position;
-    QVector<Menu*> childMenus;
-    Menu* parent;
-    int length;
+    QString menuName; //name of the menu
+    QVector<QString> menuItems; //the child menus of the menu, as a QString list of their names
+    int position; //the position of the menu
+    QVector<Menu*> childMenus; //the child menus as a Menu* list
+    Menu* parent; //the parent menu of the menu
+    int length; // the number of child menus in the menu
 };
 
 #endif // MENU_H
