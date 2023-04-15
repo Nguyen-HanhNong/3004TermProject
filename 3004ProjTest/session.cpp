5073d443e11e86cf9a2c6360a05136d713c7674c
Awwab Mahdi <54146519+obliviance@users.noreply.github.com>
2023-04-15 14:20:53 -0400

    Added session.cpp comments

--- 3004ProjTest/session.cpp
+++ 3004ProjTest/session.cpp
@@ -1,19 +1,24 @@
 #include "session.h"
 
+//Initializing the static member variable
 int Session::uniqueSessionID = 1;
 
-Session::Session(QWidget* p, int challengeLevel): QWidget(p), challengeLevel(challengeLevel), length(0), reader(Reader()) 
+//Default constructor
+Session::Session(QWidget* p, int challengeLevel): QWidget(p), challengeLevel(challengeLevel), length(0), reader(Reader())
 {
     this->timeCreated = QDateTime::currentDateTime().toString("yyyy-MM-dd hh:mm:ss");
     this->sessionID = uniqueSessionID;
     uniqueSessionID++;
 }
 
+//Default destructor
 Session::~Session() {
 
 }
 
+//Function to calculate and store the values of all unevaluated member variables
 void Session::calculateSummaryData() {
+    //Evaluating averageCoherenceScore
     if(reader.getNumberOfAchievementUpdates() == 0) {
         averageCoherenceScore = 0;
     }
@@ -21,6 +26,7 @@ void Session::calculateSummaryData() {
         averageCoherenceScore = reader.getLatestAchievementScore() / reader.getNumberOfAchievementUpdates();
     }
 
+    //Evaluating percentageOfHighCoherence
     if(getTimeAtHigh() == 0.0) {
         percentageOfHighCoherence = 0.0;
     }
@@ -28,6 +34,7 @@ void Session::calculateSummaryData() {
         percentageOfHighCoherence = (getTimeAtHigh() / length) * 100;
     }
 
+    //Evaluating percentageOfMediumCoherence
     if(getTimeAtMedium() == 0.0) {
         percentageOfMediumCoherence = 0.0;
     }
@@ -35,56 +42,56 @@ void Session::calculateSummaryData() {
         percentageOfMediumCoherence = (getTimeAtMedium() / length) * 100;
     }
 
+    //Evaluating percentageOfLowCoherence
     if(getTimeAtLow() == 0.0) {
         percentageOfLowCoherence = 0.0;
     }
     else {
         percentageOfLowCoherence = (getTimeAtLow() / length) * 100;
     }
-    
+
     return;
 }
 
+//Function to update the running session
 void Session::update() {
     this->reader.generateNextDataPoint();
     this->length += 1;
 }
 
-// getters
+// GETTERS
+
+//Getter for challengeLevel
 int Session::getChallengeLevel() { return challengeLevel; }
-
-double Session::getTimeAtLow() { return reader.getTimeInEachCoherence().at(2); }
-
-double Session::getTimeAtMedium() { return reader.getTimeInEachCoherence().at(1); }
-
-double Session::getTimeAtHigh() { return reader.getTimeInEachCoherence().at(0); }
-
-double Session::getCurrentCoherence() {
-    return reader.getLatestCoherenceScore();
-}
-
-int Session::getLength() { return length; } //Seconds
-
+//Getter for length(in seconds)
+int Session::getLength() { return length; }
+//Getter for achievement
 double Session::getAchievement() { return reader.getLatestAchievementScore(); }
-
+//Getter for sessionID
+int Session::getSessionID() { return sessionID; }
+//Getter for averageCoherenceScore
+double Session::getAverageCoherenceScore() { return averageCoherenceScore; }
+//Getter for percentageOfHighCoherence
+double Session::getPercentageOfHighCoherence() { return percentageOfHighCoherence; }
+//Getter for percentageOfMediumCoherence
+double Session::getPercentageOfMediumCoherence() { return percentageOfMediumCoherence; }
+//Getter for percentageOfLowCoherence
+double Session::getPercentageOfLowCoherence() { return percentageOfLowCoherence; }
+//Getter for timeCreated
+QString Session::getDateCreated() { return timeCreated; }
+//Getter for reader
 Reader Session::getReader() { return reader; }
 
-double Session::getAverageCoherenceScore() { return averageCoherenceScore; }
 
-double Session::getPercentageOfHighCoherence() { return percentageOfHighCoherence; }
+//Getter for time in low coherence value from reader
+double Session::getTimeAtLow() { return reader.getTimeInEachCoherence().at(2); }
+//Getter for time in medium coherence value from reader
+double Session::getTimeAtMedium() { return reader.getTimeInEachCoherence().at(1); }
+//Getter for time in high coherence value from reader
+double Session::getTimeAtHigh() { return reader.getTimeInEachCoherence().at(0); }
+//Getter for the latest coherence score from the reader
+double Session::getCurrentCoherence() { return reader.getLatestCoherenceScore(); }
 
-double Session::getPercentageOfMediumCoherence() { return percentageOfMediumCoherence; }
 
-double Session::getPercentageOfLowCoherence() { return percentageOfLowCoherence; }
-
-QString Session::getCoherenceLevel() {
-    return reader.getCoherenceLevel(this->challengeLevel);
-}
-
-QString Session::getDateCreated() {
-    return timeCreated;
-}
-
-int Session::getSessionID() {
-    return sessionID;
-}
+//Function that returns the value of reader.getCoherenceLevel() for the value of challenge level currently being used
+QString Session::getCoherenceLevel() { return reader.getCoherenceLevel(this->challengeLevel); }
